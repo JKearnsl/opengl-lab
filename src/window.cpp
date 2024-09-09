7ec49d28964336ceddc8f26f661493c06a24dff6
JKearnsl <boyarshinovn@gmail.com>
2024-09-09 13:51:31 +0300

    update: added Window type

--- src/window.cpp
+++ src/window.cpp
@@ -3,3 +3,46 @@
 //
 
 #include "window.h"
+#include <iostream>
+
+Window::Window(const char* title, const int width, const int height) {
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        exit(-1);
+    }
+
+    window = glfwCreateWindow(width, height, title, nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        exit(-1);
+    }
+
+    glfwMakeContextCurrent(window);
+    glViewport(0, 0, width, height);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    framebuffer_size_callback(window, width, height);
+}
+
+Window::~Window() {
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
+
+void Window::loop(void (*renderFunc)()) const {
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glLoadIdentity();
+        renderFunc();
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+}
+
+void Window::framebuffer_size_callback(GLFWwindow* window, const int width, const int height) {
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0, width, 0.0, height, -1.0, 1.0);
+    glMatrixMode(GL_MODELVIEW);
+}
--- src/window.h
+++ src/window.h
@@ -5,4 +5,18 @@
 #ifndef WINDOW_H
 #define WINDOW_H
 
+#include <GLFW/glfw3.h>
+
+class Window {
+public:
+    Window(const char* title, int width, int height);
+    ~Window();
+
+    void loop(void (*renderFunc)()) const;
+
+private:
+    GLFWwindow* window;
+
+    static void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+};
 #endif //WINDOW_H
