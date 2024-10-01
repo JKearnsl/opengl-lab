304ef3b6f320132e402fe964f3d824427db2e3f8
JKearnsl <boyarshinovn@gmail.com>
2024-10-01 14:51:06 +0300

    update: changed 2d circle to 3d ring

--- src/lab2/main.cpp
+++ src/lab2/main.cpp
@@ -4,21 +4,6 @@
 #include "window.h"
 
 
-void setup_lighting() {
-    glEnable(GL_LIGHTING);
-    glEnable(GL_LIGHT0);
-
-    GLfloat light_position[] = { 1.0f, 1.0f, 1.0f, 0.0f };
-    GLfloat light_ambient[] = { 0.2f, 0.2f, 0.2f, 1.0f };
-    GLfloat light_diffuse[] = { 0.8f, 0.8f, 0.8f, 1.0f };
-    GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-}
-
 void set_material_color(unsigned int rgba) {
     GLfloat material_color[] = {
         static_cast<float>((rgba >> 24) & 0xff) / 255.0f,
@@ -29,43 +14,65 @@ void set_material_color(unsigned int rgba) {
     glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, material_color);
 }
 
-void draw_circle(
-    float x,
-    float y,
-    float r,
-    unsigned int fill_rgba = 0xff000000,
-    unsigned int border_rgba = 0x000000FF,
-    unsigned int border_width = 5
-) {
+
+void draw_ring(
+        float x,
+        float y,
+        float z,
+        float r,
+        float dr,
+        float depth = 10,
+        unsigned int color_rgba = 0x000000FF
+    ) {
     glPushMatrix();
-    glTranslatef(x, y, 0.0f);
-    unsigned int border_radius = r - border_width;
+    glTranslatef(x, y, z);
+
+    float ring_inner_radius = r;
+    float ring_outer_radius = r + dr;
 
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    glBegin(GL_TRIANGLE_FAN);
+    set_material_color(color_rgba);
 
-    set_material_color(fill_rgba);
+    for (int i = 0; i < 360; i++) {
+        float angle1 = (M_PI * i / 180.0f);
+        float angle2 = (M_PI * (i + 1) / 180.0f);
 
-    // Draw inner circle
-    glBegin(GL_TRIANGLE_FAN);
-    glVertex2f(0, 0);
-    for (int i = 0; i <= 360; i++) {
-        float angle = 3.1415926f * i / 180;
-        glVertex2f(r * cos(angle), r * sin(angle));
+        // Внешняя часть кольца
+        glBegin(GL_QUADS);
+        // Нижняя грань
+        glVertex3f(ring_outer_radius * cos(angle1), ring_outer_radius * sin(angle1), 0);
+        glVertex3f(ring_outer_radius * cos(angle2), ring_outer_radius * sin(angle2), 0);
+        // Верхняя грань
+        glVertex3f(ring_outer_radius * cos(angle2), ring_outer_radius * sin(angle2), depth);
+        glVertex3f(ring_outer_radius * cos(angle1), ring_outer_radius * sin(angle1), depth);
+        glEnd();
+
+        // Внутренняя часть кольца
+        glBegin(GL_QUADS);
+        // Нижняя грань
+        glVertex3f(ring_inner_radius * cos(angle1), ring_inner_radius * sin(angle1), 0);
+        glVertex3f(ring_inner_radius * cos(angle2), ring_inner_radius * sin(angle2), 0);
+        // Верхняя грань
+        glVertex3f(ring_inner_radius * cos(angle2), ring_inner_radius * sin(angle2), depth);
+        glVertex3f(ring_inner_radius * cos(angle1), ring_inner_radius * sin(angle1), depth);
+        glEnd();
+
+        // Стороны между внутренним и внешним кольцом
+        glBegin(GL_QUADS);
+
+        glVertex3f(ring_inner_radius * cos(angle1), ring_inner_radius * sin(angle1), 0);
+        glVertex3f(ring_inner_radius * cos(angle2), ring_inner_radius * sin(angle2), 0);
+        glVertex3f(ring_outer_radius * cos(angle2), ring_outer_radius * sin(angle2), 0);
+        glVertex3f(ring_outer_radius * cos(angle1), ring_outer_radius * sin(angle1), 0);
+
+        glVertex3f(ring_inner_radius * cos(angle1), ring_inner_radius * sin(angle1), depth);
+        glVertex3f(ring_inner_radius * cos(angle2), ring_inner_radius * sin(angle2), depth);
+        glVertex3f(ring_outer_radius * cos(angle2), ring_outer_radius * sin(angle2), depth);
+        glVertex3f(ring_outer_radius * cos(angle1), ring_outer_radius * sin(angle1), depth);
+        glEnd();
     }
-    glEnd();
-
-    // Draw border circle
-    glBegin(GL_TRIANGLE_STRIP);
-    set_material_color(border_rgba);
-    for (int i = 0; i <= 360; i++) {
-        float angle = 3.1415926f * i / 180;
-        glVertex2f(border_radius * cos(angle), border_radius * sin(angle));
-        glVertex2f(r * cos(angle), r * sin(angle));
-    }
-    glEnd();
 
     glPopMatrix();
 }
@@ -73,14 +80,17 @@ void draw_circle(
 
 int main() {
     const Window window("Lab 2", 800, 600);
-    setup_lighting();
-    window.loop([] {
-        draw_circle(415, 400, 50, 0xffffff00, 0xF29E00FF);
-        draw_circle(540, 400, 50, 0xffffff00, 0x009E24FF);
+    Window::setup_lighting();
 
-        draw_circle(350, 450, 50, 0xffffff00, 0x00A2C5FF);
-        draw_circle(475, 450, 50, 0xffffff00, 0x010101FF);
-        draw_circle(600, 450, 50, 0xffffff00, 0xFF0000FF);
+    window.loop([](GLFWwindow*) {
+
+        draw_ring(-30, -10, 0, 10, 3, 4, 0x005BBFff);
+        draw_ring(0, -10, 0, 10, 3, 4, 0x000000FF);
+        draw_ring(30, -10, 0, 10, 3, 4, 0xFF0000FF);
+
+        draw_ring(-15, -20, 0, 10, 3, 4, 0xF29E00FF);
+        draw_ring(15, -20, 0, 10, 3, 4, 0x009E24FF);
+
     });
 
     return 0;
